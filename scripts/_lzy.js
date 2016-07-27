c82677891e396fda10f34f74320b19e23dc57d00
DaveCC94 <david.castro.cisneros@gmail.com>
2016-07-26 22:42:09 -0700

    D:Index is modified, it now get posts. Added angular route and fixed a minor bug on image entry

--- scripts/_lzy.js
+++ scripts/_lzy.js
@@ -1,5 +1,15 @@
-angular.module("luzenly", []).config(
-		function($sceDelegateProvider) {
+angular.module("luzenly", ['ngRoute']).config(['$routeProvider','$sceDelegateProvider',
+  function($routeProvider, $sceDelegateProvider) {    
 			$sceDelegateProvider.resourceUrlWhitelist([ 'self',
 					'*://www.youtube.com/**' ]);
-		});
+
+	$routeProvider.
+      when('/getContents', {
+        templateUrl: '/luzenly/templates/lzyGetContents.html',
+        controller: 'lzyGetContentsCtrl'
+      }).
+      otherwise({
+        redirectTo: '/getContents'
+      });
+  }]);
+
